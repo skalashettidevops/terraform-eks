39dbb5d5e7d240ff25f7f5ed45986258083ec524
skalashettidevops <150411969+skalashettidevops@users.noreply.github.com>
2025-06-19 07:29:05 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,30 +1,29 @@
-
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 1.3.0"
   required_providers {
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.1.0"
+      version = "~> 3.4"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">=2.7.1"
+      version = "~> 2.15"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.68.0"
+      version = "~> 5.40"
     }
     local = {
       source  = "hashicorp/local"
-      version = "~> 2.1.0"
+      version = "~> 2.4"
     }
     null = {
       source  = "hashicorp/null"
-      version = "~> 3.1.0"
+      version = "~> 3.4"
     }
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.2.0"
+      version = "~> 2.4"
     }
   }
 }
